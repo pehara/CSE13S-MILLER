e099338d815cd626a4cfa2121e27f81cb3539b48
pehara <pvidanga@ucsc.edu>
2022-10-15 17:04:31 -0700

    adding back

---
+++ asgn3.1/dreidel.h
@@ -0,0 +1,12 @@
+//Should contain function declarations
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define MAX_NUM_PLAYERS 8
+
+int play_game(int n_players, int coins_per_player, int * n_rounds);
+
+extern uint64_t game_seed;
+
+extern int print_message;
