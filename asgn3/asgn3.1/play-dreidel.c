771f1c02f19122abdda06914b5b42c2fa8c0c617
pehara <pvidanga@ucsc.edu>
2022-10-15 21:24:18 -0700

    files

---
+++ asgn3/asgn3.1/play-dreidel.c
@@ -0,0 +1,54 @@
+//The CLI, it should interface with the game but not contain any game logic
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+#include "dreidel.h"
+
+#define OPTIONS "p:c:s:v"
+
+uint64_t game_seed;
+int print_message;
+int opt;
+
+int main (int argc, char **argv) {
+        int n_players = 5;
+        int coins_per_player = 6;
+        int n_rounds;
+        
+        game_seed = 613;
+
+        print_message = 0;
+
+        while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+                for (optind = 1; optind < argc; optind++){
+                    
+                        if (!strcmp(argv[optind], "-p")) {
+                                n_players = atoi(argv[optind + 1]);
+                        }
+
+                        if (!strcmp(argv[optind], "-c")) {
+                                coins_per_player = atoi(argv[optind + 1]);
+                        }
+
+                        if (!strcmp(argv[optind], "-s")) {
+                                game_seed = strtoul(argv[optind + 1], NULL, 0);
+                        }
+
+                        if (!strcmp(argv[optind], "-v")) {
+                                print_message = 1;
+                        }
+                }
+        }
+
+        if (n_players > 8 || n_players < 2 || coins_per_player > 20 || coins_per_player < 1 || game_seed < 1 ) {
+                printf ("\nerror invalid entry. \n");
+                return 1;
+        }
+        else {
+                play_game (n_players, coins_per_player, &n_rounds);
+                return 0;
+        }
+        
+}
---
+++ asgn3/dreidel.c
@@ -0,0 +1,87 @@
+//Should store the game logic
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "mtrand.h"
+#include "dreidel.h"
+#include <stdint.h>
+
+char player_names[8][9] = {"Aharon","Batsheva","Chanah","David","Ephraim","Faige","Gamaliel","Hannah"};
+
+char dreidel_faces[] = {'N','G','H','S'};
+
+int num_coins_in_hand[MAX_NUM_PLAYERS];
+
+
+char spin_dreidel(void) {
+	int ran_num;
+	
+	ran_num = mtrand_rand64() % 4;
+	return dreidel_faces[ran_num];
+}
+
+
+int play_game (int n_players, int coins_per_player, int * n_rounds) {
+	int i;
+	int r;
+	int p;
+	int pot;
+	int remaing_players;
+	char spinspin;
+	
+	pot = 0;
+
+	mtrand_seed (game_seed);
+	//mtrand_seed(707);	
+
+	for (i = 0; i < n_players; ++i) { 
+		//intialize player with num coins
+		num_coins_in_hand[i] = coins_per_player;
+	}
+
+	remaing_players = n_players;
+	r = 0;
+
+	while (remaing_players != 1) {
+		for (p = 0; p < n_players; ++p) {
+			if (num_coins_in_hand[p] != 0) {
+				spinspin = spin_dreidel();
+			
+				switch (spinspin) {
+					case 'N':
+						num_coins_in_hand[p] = num_coins_in_hand[p];
+						break;
+					case 'G':
+						num_coins_in_hand[p] = num_coins_in_hand[p] + pot;
+						pot = 0;
+						break;
+					case 'H':
+						num_coins_in_hand[p] = num_coins_in_hand[p] + pot/2;
+						pot = pot - pot/2;
+						break;
+					default: //'S'
+						num_coins_in_hand[p] = num_coins_in_hand[p] - 1;
+						pot = pot + 1;
+				}
+
+				if (num_coins_in_hand[p] == 0) {
+					remaing_players = remaing_players - 1;
+					if (print_message == 1) {
+						printf ("%s: eliminated in round %d of a %d player game.\n", player_names[p], r, n_players);
+					}
+				}
+			}
+		}
+		r = r + 1; //next game
+	}
+
+	for (p = 0; p < n_players; ++p) {
+		if (num_coins_in_hand[p] != 0) {
+			printf("\n%s %d %d %d %lu \n", player_names[p], n_players, coins_per_player, r, game_seed);
+		}
+	}
+
+	*n_rounds = r;
+
+	return 1;
+}
